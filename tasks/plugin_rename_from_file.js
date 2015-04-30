0392d91b24e44a37d7492c3b3ccffbcdb6485059
Emma Chee <Chee.Emma@abc.net.au>
2015-04-30 16:39:13 +1000

    initial commit

---
+++ tasks/plugin_rename_from_file.js
@@ -0,0 +1,46 @@
+/*
+ * grunt-plugin-rename-from-file
+ * https://github.com/cheee6y/grunt-plugin-rename-from-file
+ *
+ * Copyright (c) 2015 Emma Chee
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('plugin_rename_from_file', 'batch rename files by mapping them to a supplied list of names. Intended to renamed a bunch of image files that corresponded to a random name.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(file) {
+      var namesList = [];
+      var dest = file.dest;
+      var names = file.names;
+      var jsonfile = grunt.file.readJSON(names);
+      var images = grunt.file.expand(file.images);
+
+      grunt.file.delete(dest);
+
+      //create an array to hold the names of the md5's
+      for (var i=0; i<jsonfile.length; i++) {
+        namesList.push(jsonfile[i].md5);
+      }
+
+      // copy the images to the destination folder with an md5 name
+      for (var j=0; j<namesList.length; j++) {
+        grunt.file.copy(images[j], dest + namesList[j] +'.jpg')
+        grunt.log.writeln('File "' + namesList[j] +'.jpg" created.');
+      }
+    });
+  });
+
+};
---
+++ test/plugin_rename_from_file_test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var grunt = require('grunt');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.plugin_rename_from_file = {
+  setUp: function(done) {
+    // setup here if necessary
+    done();
+  },
+  default_options: function(test) {
+    var file = '35d54f6649e07b9db764998faa2bce61.jpg';
+    var actual = grunt.file.exists('test/expected/' + file);
+    var expected = grunt.file.exists('tmp/images/' + file);
+    grunt.file.exists
+    test.equal(actual, expected, 'should describe what the default behavior is.');
+
+    test.done();
+  }
+};
